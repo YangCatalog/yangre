aebf2746b13d53b365984d0a62bea34513863f76
Eric Vyncke <evyncke@cisco.com>
2019-02-03 07:29:19 +0000

    Use /tmp for temporary files rather than current directory

--- app/views.py
+++ app/views.py
@@ -49,7 +49,7 @@ def w3c():  # JSON API to validate W3C input
     req_data = request.get_json()
 
     # writing the test string to file, as required by w3cgrep
-    w3cinput_filename = "w3c_input" + str(uuid.uuid4())
+    w3cinput_filename = "/tmp/w3c_input" + str(uuid.uuid4())
     with open(w3cinput_filename, "w") as testfile:
         testfile.write(req_data['content'])
         testfile.write("\n")
@@ -93,7 +93,7 @@ def yangre():  # JSON API to validate YANG input
     req_data = request.get_json()
 
     # writing the test string to another file for yangre
-    yangreinput_filename = "yangre_input" + str(uuid.uuid4())
+    yangreinput_filename = "/tmp/yangre_input" + str(uuid.uuid4())
     with open(yangreinput_filename, "w") as yangrefile:
         yangrefile.write(str(req_data['pattern']))
         yangrefile.write("\n\n")
