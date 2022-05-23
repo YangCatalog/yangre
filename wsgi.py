f9ce736c8309c2e49f09e03c8f4f2df6df01dbf2
Slavomir Mazur <slavomir.mazur@pantheon.tech>
2022-05-23 09:42:59 +0200

    Update yangre image build
    This resolves #13
    
    Signed-off-by: Slavomir Mazur <slavomir.mazur@pantheon.tech>

--- wsgi.py
+++ wsgi.py
@@ -1,22 +1,27 @@
-#Author: Pieter Lewyllie, pilewyll@cisco.com
-
-#!/usr/bin/env python
-
 # Copyright The IETF Trust 2019, All Rights Reserved
-# Copyright (c) 2015-2018 Cisco and/or its affiliates.
+# Copyright 2018 Cisco and its affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# This software is licensed to you under the terms of the Apache License, Version 2.0 (the "License").
-# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-# The code, technical concepts, and all information contained herein, are the property of Cisco Technology, Inc.
-# and/or its affiliated entities, under various laws including copyright, international treaties, patent,
-# and/or contract. Any use of the material herein must be in accordance with the terms of the License.
-# All rights not expressly granted by the License are reserved.
-# Unless required by applicable law or agreed to separately in writing, software distributed under the
-# License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-# either express or implied.
+__author__ = 'Pieter Lewyllie'
+__copyright__ = 'Copyright 2018 Cisco and its affiliates, Copyright The IETF Trust 2019, All Rights Reserved'
+__license__ = 'Apache License, Version 2.0'
+__email__ = 'pilewyll@cisco.com'
 
-from app import app as application
+
 import config
 
-if __name__ == "__main__":
+from app import app as application
+
+if __name__ == '__main__':
     application.run()
